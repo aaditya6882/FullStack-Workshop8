f7e9f8ac95864f20ef75b89cb6417cf29f43c784
aaditya6882 <aadityaacharya6882@gmail.com>
2026-01-19 18:53:12 +0545

    week8

---
+++ Workshop8/db.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost:3307';
+$db = 'employee';
+$user = 'root';
+$pass = '';
+try {
+$pdo = new PDO(
+"mysql:host=$host;dbname=$db;charset=utf8",
+$user,
+$pass,
+[
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]
+);
+} catch (PDOException $e) {
+die("Database connection failed");
+}
